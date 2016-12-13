ca02f75d1959d0833289a7c8ae500546b3ed627d
aestrada <aestrada@AMES-LENOVO1743>
2016-12-12 22:09:01 -0600

    Created empty class for calculator and empty test class. Testing PayForTheNightCalculator constructor throws NotImplementedException.

---
+++ BabysitterKataTests/PayForTheNightCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BabysitterKata.Tests
+{
+    [TestClass()]
+    public class PayForTheNightCalculatorTests
+    {
+        [TestMethod()]
+        public void PayForTheNightCalculatorTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
